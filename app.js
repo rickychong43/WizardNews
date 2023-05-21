5da0a39aa7a60dbaf8003534257b44a81c09f30e
rickychong43 <ricky43@vt.edu>
2023-05-21 10:54:01 -0400

    update

--- app.js
+++ app.js
@@ -1,9 +1,98 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const volleyball = require('volleyball');
+const postBank = require('./postBank');
+
+
 const app = express();
+const { PORT = 3000 } = process.env;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(volleyball);
+app.use(express.static('public'));
 
-const PORT = 1337;
+
+
+
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+  
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-item">
+            ${post.content}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`;
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>404: Page Not Found</p>
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+  }
+  else{
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+  
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <p class="news-info">
+            ${post.content}
+          </p>
+        </div>
+    </div>
+  </body>
+</html>`;
+  res.send(html);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
